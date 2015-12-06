ad17b63a56d92e62a90875e5f2d7f268c27c1172
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-06 13:25:50 -0200

    support linux

---
+++ Package.swift
@@ -0,0 +1,9 @@
+import PackageDescription
+
+let package = Package(
+	name: "Epoch",
+	dependencies: [
+        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 1)
+	]
+)
