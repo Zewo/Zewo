aec259f93ed5c46bb72420c7e01ea444656a44a3
Paulo Faria <paulo.faria.rl@gmail.com>
2015-12-20 06:52:28 -0800

    fix 2015-12-18 toolchain SPM bug

--- Package.swift
+++ Package.swift
@@ -4,6 +4,9 @@ let package = Package(
 	name: "Epoch",
 	dependencies: [
 		.Package(url: "https://github.com/Zewo/HTTPParser.git", majorVersion: 0, minor: 1),
-        	.Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1)
+        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CURIParser.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 1),
+        .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 1)
 	]
 )
