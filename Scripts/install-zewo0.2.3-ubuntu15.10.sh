6204c51e8ea093cd55bbf321c45a25ee61a45551
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-25 02:55:45 -0300

    add ubuntu 15.10 install script

---
+++ Scripts/install-zewo0.2.3-ubuntu15.10.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SWIFT_URL=https://swift.org/builds/development/ubuntu1510/swift-DEVELOPMENT-SNAPSHOT-2016-02-08-a/swift-DEVELOPMENT-SNAPSHOT-2016-02-08-a-ubuntu15.10.tar.gz
+ZEWO_VERSION=0.2.3
+
+# Install apt packages
+grep -q -F "deb [trusted=yes] http://apt.zewo.io/deb ./" /etc/apt/sources.list || echo "deb [trusted=yes] http://apt.zewo.io/deb ./" | sudo tee --append /etc/apt/sources.list
+sudo apt-get update
+sudo apt-get -y install clang libicu-dev zewo="${ZEWO_VERSION}"
+
+# Install Swift
+cd ${HOME}
+wget $SWIFT_URL -O - | tar xz
+
+if [ -d .swift ]; then
+    rm -rf .swift
+fi
+
+mv $(basename "$SWIFT_URL" ".tar.gz") .swift
+export PATH="${HOME}/.swift/usr/bin:${PATH}"
+
+echo "Zewo ${ZEWO_VERSION} successfully installed!"
+echo 'You may wish to add "${HOME}/swift/usr/bin" to your PATH in your profile'
