18d70d9d0c95b2ac673f520888069eacc6cb4d9a
Paulo Faria <paulo.faria.rl@gmail.com>
2016-02-29 10:52:46 -0300

    bump to version 0.3

--- Package.swift
+++ Package.swift
@@ -9,22 +9,29 @@ import PackageDescription
 let package = Package(
     name: "Zewo",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTPSServer.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTPSClient.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/HTTPFile.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/UDP.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 2),
+        // HTTP
+        .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/HTTPSServer.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/HTTPSClient.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/HTTPFile.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 3),
+        // Middleware
+        .Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/BasicAuthMiddleware.git", majorVersion: 0, minor: 3),
+        .Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 3),
+        // Media Types
         .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/JSONMediaType.git", majorVersion: 0, minor: 3),
         .Package(url: "https://github.com/Zewo/URLEncodedForm.git", majorVersion: 0, minor: 2),
+        // Base
+        .Package(url: "https://github.com/Zewo/UDP.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 2),
+        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/ChannelStream.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 2),
-        .Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 3),
+        // Database
         .Package(url: "https://github.com/Zewo/SQL.git", majorVersion: 0, minor: 2),
+        // C dependencies
         .Package(url: openSSLURL, majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CLibvenice.git", majorVersion: 0, minor: 2),
         .Package(url: "https://github.com/Zewo/CHTTPParser.git", majorVersion: 0, minor: 2),
