e4e76ba36adc79a6fcd5ce07bf5ee480a5ae8f0f
Dan Appel <dan.appel00@gmail.com>
2016-03-26 16:25:13 -0700

    Remove commented lines (they confuse zewo-dev)

--- Package.swift
+++ Package.swift
@@ -5,9 +5,6 @@ let package = Package(
     dependencies: [
         // HTTP
         .Package(url: "https://github.com/Zewo/HTTPServer.git", majorVersion: 0, minor: 4),
-        // .Package(url: "https://github.com/Zewo/HTTPClient.git", majorVersion: 0, minor: 4),
-        // .Package(url: "https://github.com/Zewo/HTTPSServer.git", majorVersion: 0, minor: 4),
-        // .Package(url: "https://github.com/Zewo/HTTPSClient.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/HTTPFile.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 4),
         // Middleware
@@ -20,7 +17,6 @@ let package = Package(
         .Package(url: "https://github.com/Zewo/URLEncodedForm.git", majorVersion: 0, minor: 4),
         // Base
         .Package(url: "https://github.com/Zewo/Base64.git", majorVersion: 0, minor: 4),
-        // .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 4),
         .Package(url: "https://github.com/Zewo/ChannelStream.git", majorVersion: 0, minor: 4),
     ]
 )
