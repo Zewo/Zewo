17866e43c67d3b8388bf872ddfe69b712ece533d
Paulo Faria <paulo.faria.rl@gmail.com>
2016-04-19 18:21:58 -0300

    bump to 0.5.0

--- Package.swift
+++ Package.swift
@@ -4,21 +4,21 @@ let package = Package(
     name: "Zewo",
     dependencies: [
         // HTTP
-        .Package(url: "https://github.com/VeniceX/HTTPServer.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/VeniceX/HTTPFile.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/VeniceX/HTTPServer.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/VeniceX/HTTPClient.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/VeniceX/HTTPFile.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/Router.git", majorVersion: 0, minor: 5),
         // Middleware
-        .Package(url: "https://github.com/Zewo/RecoveryMiddleware.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/BasicAuthMiddleware.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/RecoveryMiddleware.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/LogMiddleware.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/BasicAuthMiddleware.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/ContentNegotiationMiddleware.git", majorVersion: 0, minor: 5),
         // Media Types
-        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/JSONMediaType.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/URLEncodedForm.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/URLEncodedFormMediaType.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/JSON.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/JSONMediaType.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/URLEncodedForm.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/URLEncodedFormMediaType.git", majorVersion: 0, minor: 5),
         // Other
-        .Package(url: "https://github.com/VeniceX/ChannelStream.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/VeniceX/ChannelStream.git", majorVersion: 0, minor: 5),
     ]
 )
