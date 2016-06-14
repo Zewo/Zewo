d3a0a4a54900e77a7e58c58e8c78f68285e408ce
Dan Appel <dan.appel00@gmail.com>
2016-06-14 01:10:09 -0700

    Clean up swift-install script

--- Scripts/install-swift.sh
+++ Scripts/install-swift.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# Do nothing on Darwin-based systems
 if [[ "$(uname)" == "Linux" ]]; then
     SWIFT_URL=https://swift.org/builds/swift-3.0-preview-1/ubuntu1404/swift-3.0-preview-1/swift-3.0-preview-1-ubuntu14.04.tar.gz
     BASE_DIR=$(pwd)
@@ -7,16 +8,17 @@ if [[ "$(uname)" == "Linux" ]]; then
     # Install Swift
     cd ${HOME}
     wget $SWIFT_URL -O - | tar xz
+
     # Make sure Swift is not already installed
     if [ -d .swift ]; then
         rm -rf .swift
     fi
+
     # Move to .swift, set PATH
     mv $(basename "$SWIFT_URL" ".tar.gz") .swift
     export PATH="${HOME}/.swift/usr/bin:${PATH}"
-else
-    BASE_DIR=$(pwd)
+
+    # Move back to where we started
+    cd $BASE_DIR
 fi
 
-# Move back to where we started
-cd $BASE_DIR
