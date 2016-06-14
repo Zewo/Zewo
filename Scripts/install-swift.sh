d77bb9a59123893cdc37396887f453f9fee7b359
Dan Appel <dan.appel00@gmail.com>
2016-06-14 09:46:42 -0700

    Use swiftenv for swift install script

--- Scripts/install-swift.sh
+++ Scripts/install-swift.sh
@@ -2,23 +2,19 @@
 
 # Do nothing on Darwin-based systems
 if [[ "$(uname)" == "Linux" ]]; then
-    SWIFT_URL=https://swift.org/builds/swift-3.0-preview-1/ubuntu1404/swift-3.0-preview-1/swift-3.0-preview-1-ubuntu14.04.tar.gz
-    BASE_DIR=$(pwd)
 
-    # Install Swift
-    cd ${HOME}
-    wget $SWIFT_URL -O - | tar xz
+  # Taken from https://gist.githubusercontent.com/kylef/5c0475ff02b7c7671d2a/raw/02090c7ede5a637b76e6df1710e83cd0bbe7dcdf/swiftenv-install.sh
+  # Automatically installs swiftenv and run's swiftenv install.
+  # This script was designed for usage in CI systems.
 
-    # Make sure Swift is not already installed
-    if [ -d .swift ]; then
-        rm -rf .swift
-    fi
+  git clone --depth 1 https://github.com/kylef/swiftenv.git ~/.swiftenv
+  export SWIFTENV_ROOT="$HOME/.swiftenv"
+  export PATH="$SWIFTENV_ROOT/bin:$SWIFTENV_ROOT/shims:$PATH"
 
-    # Move to .swift, set PATH
-    mv $(basename "$SWIFT_URL" ".tar.gz") .swift
-    export PATH="${HOME}/.swift/usr/bin:${PATH}"
-
-    # Move back to where we started
-    cd $BASE_DIR
+  if [ -f ".swift-version" ] || [ -n "$SWIFT_VERSION" ]; then
+    swiftenv install
+  else
+    swiftenv rehash
+  fi
 fi
 
