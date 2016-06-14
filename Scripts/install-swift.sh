cc82ec9dc11ec6001753439223dc67d0cd4ae1f7
Dan Appel <dan.appel00@gmail.com>
2016-06-13 23:02:27 -0700

    Add install-swift script (swift-3.0-preview-1)

---
+++ Scripts/install-swift.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+SWIFT_URL=https://swift.org/builds/swift-3.0-preview-1/ubuntu1404/swift-3.0-preview-1/swift-3.0-preview-1-ubuntu14.04.tar.gz
+
+# Install Swift
+cd ${HOME}
+wget $SWIFT_URL -O - | tar xz
+
+# Make sure Swift is not already installed
+if [ -d .swift ]; then
+    rm -rf .swift
+fi
+
+# Move to .swift, set PATH
+mv $(basename "$SWIFT_URL" ".tar.gz") .swift
+export PATH="${HOME}/.swift/usr/bin:${PATH}"
