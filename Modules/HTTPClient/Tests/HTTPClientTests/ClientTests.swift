37fe629513e216d0fb2bd5e00807a34e8703cdde
Paulo Faria <paulo.faria.rl@gmail.com>
2016-09-18 19:39:12 -0300

    remove extra tests from HTTPClient

---
+++ Modules/HTTPClient/Tests/HTTPClientTests/ClientTests.swift
@@ -0,0 +1,10 @@
+import XCTest
+@testable import HTTPServer
+
+public class ClientTests : XCTestCase {}
+
+extension ClientTests {
+    public static var allTests: [(String, (ClientTests) -> () throws -> Void)] {
+        return []
+    }
+}
--- Modules/HTTPClient/Tests/LinuxMain.swift
+++ Modules/HTTPClient/Tests/LinuxMain.swift
@@ -1,66 +1,7 @@
 import XCTest
-import CoreTests
-import FileTests
-// import HTTPClientTests
-// import HTTPFileTests
-import HTTPServerTests
-import HTTPTests
-import IPTests
-// import OpenSSLTests
-import POSIXTests
-import ReflectionTests
-import TCPTests
-import VeniceTests
+import HTTPClientTests
+
 
 XCTMain([
-    // POSIX
-    testCase(POSIXTests.allTests),
-    testCase(EnvironmentTests.allTests),
-
-    // Venice
-    testCase(ChannelTests.allTests),
-    testCase(CoroutineTests.allTests),
-    testCase(FallibleChannelTests.allTests),
-    testCase(FileTests.allTests),
-    testCase(IPTests.allTests),
-    testCase(SelectTests.allTests),
-    testCase(TCPTests.allTests),
-    testCase(TickerTests.allTests),
-    testCase(TimerTests.allTests),
-
-    // Core
-    testCase(JSONTests.allTests),
-    testCase(LoggerTests.allTests),
-    testCase(InternalTests.allTests),
-    testCase(MapConvertibleTests.allTests),
-    testCase(PerformanceTests.allTests),
-    testCase(PublicTests.allTests),
-    testCase(StringTests.allTests),
-    testCase(MapTests.allTests),
-    testCase(URLEncodedFormParserTests.allTests),
-
-
-    // HTTP
-    testCase(RequestContentTests.allTests),
-    testCase(ResponseContentTests.allTests),
-    testCase(AttributedCookieTests.allTests),
-    testCase(BodyTests.allTests),
-    testCase(CookieTests.allTests),
-    testCase(MessageTests.allTests),
-    testCase(RequestTests.allTests),
-    testCase(ResponseTests.allTests),
-    testCase(BasicAuthMiddlewareTests.allTests),
-    testCase(ContentNegotiationMiddlewareTests.allTests),
-    testCase(LogMiddlewareTests.allTests),
-    testCase(RecoveryMiddlewareTests.allTests),
-    testCase(RedirectMiddlewareTests.allTests),
-    testCase(SessionMiddlewareTests.allTests),
-    testCase(RequestParserTests.allTests),
-    testCase(ResponseParserTests.allTests),
-    testCase(ResourceTests.allTests),
-    testCase(RouterTests.allTests),
-    testCase(RoutesTests.allTests),
-    testCase(TrieRouteMatcherTests.allTests),
-    testCase(HTTPSerializerTests.allTests),
-    testCase(ServerTests.allTests),
+    testCase(ClientTests.allTests),
 ])
