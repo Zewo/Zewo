8066e0be5f8169315826fef3a7de891d01ebed1a
Paulo Faria <paulo.faria.rl@gmail.com>
2016-09-19 23:11:56 -0300

    add fix for tests in Xcode project generation script

--- Scripts/generate-xcodeproj.sh
+++ Scripts/generate-xcodeproj.sh
@@ -1,2 +1,3 @@
 swift package generate-xcodeproj
 sed -i '' -E "s/(path = 'Sources\/)([^\']+)(')/path = 'Modules\/\2\/Sources\/\2'/" Zewo.xcodeproj/project.pbxproj
+sed -i '' -E "s/(path = 'Tests\/)([^\']+)(Tests')/path = 'Modules\/\2\/Tests\/\2Tests'/" Zewo.xcodeproj/project.pbxproj
