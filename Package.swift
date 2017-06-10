8732bdf1b4c1f35c2691c314c5ee59db70f12fcc
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-10 16:32:27 -0300

    remove crypto code

--- Package.swift
+++ Package.swift
@@ -5,20 +5,16 @@ import PackageDescription
 let package = Package(
     name: "Zewo",
     targets: [
-        Target(name: "CHTTPParser"),
         Target(name: "CYAJL"),
-        Target(name: "CArgon2"),
+        Target(name: "CHTTPParser"),
         
         Target(name: "Core"),
-        Target(name: "Content", dependencies: ["CYAJL", "Core"]),
-        Target(name: "Crypto", dependencies: ["Core", "CArgon2"]),
         Target(name: "IO", dependencies: ["Core"]),
-        Target(name: "JWT", dependencies: ["Crypto", "Content"]),
+        Target(name: "Content", dependencies: ["CYAJL", "Core"]),
         Target(name: "HTTP", dependencies: ["Content", "IO", "CHTTPParser"]),
     ],
     dependencies: [
         .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 19),
-        .Package(url: "https://github.com/Zewo/CLibreSSL.git", majorVersion: 3),
         .Package(url: "https://github.com/Zewo/CBtls.git", majorVersion: 1),
     ]
 )
