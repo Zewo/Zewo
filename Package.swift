e62d97f08d4fcab6d0f70d834b0bf93935b93012
Paulo Faria <paulo.faria.rl@gmail.com>
2017-06-13 15:46:30 -0300

    add Zewo supermodule

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "Zewo",
     products: [
-        .library(name: "Zewo", targets: ["Core", "IO", "Media", "HTTP"])
+        .library(name: "Zewo", targets: ["Core", "IO", "Media", "HTTP", "Zewo"])
     ],
     dependencies: [
         .package(url: "https://github.com/Zewo/CLibdill.git", .branch("swift-4")),
@@ -21,6 +21,7 @@ let package = Package(
         .target(name: "IO", dependencies: ["Core"]),
         .target(name: "Media", dependencies: ["CYAJL", "Core"]),
         .target(name: "HTTP", dependencies: ["Media", "IO", "CHTTPParser"]),
+        .target(name: "Zewo", dependencies: ["Core", "IO", "Media", "HTTP"]),
 
         .testTarget(name: "CoreTests", dependencies: ["Core"]),
         .testTarget(name: "IOTests", dependencies: ["IO"]),
---
+++ Sources/Zewo/Zewo.swift
@@ -0,0 +1,4 @@
+@_exported import Core
+@_exported import IO
+@_exported import Media
+@_exported import HTTP
