f4121096a14e2fefb291c99f1ea9dd1bcec09dea
David Ask <david@formbound.com>
2017-06-13 15:33:40 +0200

    Updated swift tools version

--- Package.swift
+++ Package.swift
@@ -1,21 +1,34 @@
-// swift-tools-version:3.1
+// swift-tools-version:4.0
 
 import PackageDescription
 
 let package = Package(
     name: "Zewo",
-    targets: [
-        Target(name: "CYAJL"),
-        Target(name: "CHTTPParser"),
-        
-        Target(name: "Core"),
-        Target(name: "IO", dependencies: ["Core"]),
-        Target(name: "Media", dependencies: ["CYAJL", "Core"]),
-        Target(name: "HTTP", dependencies: ["Media", "IO", "CHTTPParser"]),
+    products: [
+        .library(
+            name: "Zewo",
+            type: .dynamic,
+            targets: [
+                "Core",
+                "IO",
+                "Media",
+                "HTTP"
+            ]
+        )
     ],
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 19),
-        .Package(url: "https://github.com/Zewo/CBtls.git", majorVersion: 1),
-        .Package(url: "https://github.com/Zewo/CLibreSSL.git", majorVersion: 3),
+        .package(url: "https://github.com/Zewo/CLibdill.git", .branch("swift-4")),
+        .package(url: "https://github.com/Zewo/Venice.git", .branch("swift-4")),
+        .package(url: "https://github.com/Zewo/CBtls.git", .branch("swift-4")),
+        .package(url: "https://github.com/Zewo/CLibreSSL.git", .branch("swift-4")),
+    ],
+    targets: [
+        .target(name: "CYAJL", dependencies: []),
+        .target(name: "CHTTPParser"),
+        
+        .target(name: "Core", dependencies: ["Venice"]),
+        .target(name: "IO", dependencies: ["Core"]),
+        .target(name: "Media", dependencies: ["CYAJL", "Core"]),
+        .target(name: "HTTP", dependencies: ["Media", "IO", "CHTTPParser"]),
     ]
 )
